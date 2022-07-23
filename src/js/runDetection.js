2ba96143aeacdd2406dc7b3b75024786ccc3ca2d
Uwe Jettkant <ujettkant@gmail.com>
2022-07-23 18:36:43 +0200

    added dataLayer key-values
    Signed-off-by: Uwe Jettkant <ujettkant@gmail.com>

--- src/js/runDetection.js
+++ src/js/runDetection.js
@@ -2,10 +2,12 @@
   var enabledEl = document.getElementById("adb-enabled");
   var disabledEl = document.getElementById("adb-not-enabled");
   function adBlockDetected() {
+    dataLayer.push({ adBlock: true, event: "adBlockStatus" });
     enabledEl.style.display = "block";
     disabledEl.style.display = "none";
   }
   function adBlockNotDetected() {
+    dataLayer.push({ adBlock: false, event: "adBlockStatus" });
     disabledEl.style.display = "block";
     enabledEl.style.display = "none";
   }
